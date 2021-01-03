53782dbc624b9e680b26e6916751e25fdcca5ba0
abenhamouda <abenhamouda@iobeya.com>
2021-01-03 11:05:23 +0100

    pretify graph plot

--- ga.py
+++ ga.py
@@ -21,7 +21,7 @@ class GeneticAlgorithm:
 
     def _nbItemsInChromosome(self, chromosome):
         return [i for i in chromosome if i == 1].__len__()
-
+    
     def printFittest(self):
         fittestOne = self.fittest()
         nb_items = self._nbItemsInChromosome(fittestOne)
@@ -29,17 +29,24 @@ class GeneticAlgorithm:
 
         for (i,(selected, item)) in enumerate(zip(self.fittest(), self.data)):
             if selected:
-                print("\titem {}: weight={} volume={} price={} name={}".format(i, item[0], item[1], item[2], item[3]))
+                print("\titem index{}: weight={} volume={} price={} name={}".format(i, item[0], item[1], item[2], item[3]))
 
     def printEvolutionGraph(self):
         _, ax = plt.subplots(figsize=(10, 6))
         xAxis = [iteration for (iteration, _) in self.evolutionGraph]
         yAxis = [self._fitness(chromosome) for (_, chromosome) in self.evolutionGraph]
+        #markerSize = [self._nbItemsInChromosome(c)**2 for (_, c) in self.evolutionGraph]
         
-        ax.scatter(x = xAxis, y = yAxis)
+        ax.scatter(x = xAxis, y = yAxis, alpha=0.5)
+        # print number of item for each solution (point)
+        #for (i, chromosome) in self.evolutionGraph:
+        #    plt.text(i, self._fitness(chromosome), self._nbItemsInChromosome(chromosome), horizontalalignment='center', verticalalignment='center', fontdict={'color':'white', 'size': 7})
+
         plt.xlabel("Iteration")
         plt.ylabel("Total value of knapsack's items")
-
+        ax.set_yticks(yAxis, minor=True)
+        plt.grid(linestyle='--', alpha=0.5)
+        plt.plot(xAxis, yAxis)
         plt.show()
 
     def _genChromosome(self):
@@ -146,17 +153,16 @@ class GeneticAlgorithm:
 
 if __name__ == "__main__":
     # setup data (weight, volume, price, name)
-    data = [(821, 0.8, 118, 'A'), (1144, 1, 322, 'B'), (634, 0.7, 166, 'C'), (701, 0.9, 195, 'D'),
-            (291, 0.9, 100, 'E'), (1702, 0.8, 142, 'F'), (1633, 0.7, 100, 'G'), (1086, 0.6, 145, 'H'),
-            (124, 0.6, 100, 'J'), (718, 0.9, 208, 'K'), (976, 0.6, 100, 'L'), (1438, 0.7, 312, 'M'),
-            (910, 1, 198, 'W'), (148, 0.7, 171, 'S'), (1636, 0.9, 117, 'V'), (237, 0.6, 100, 'N'),
+    data = [(821, 0.8, 118, 'A'), (1144, 1, 322, 'B'), (634, 0.7, 166, 'C'), (701, 0.9, 195, 'D'),(53, 0.7, 30, 'TTSS'),(88, 0.1, 5, 'TSAST'),
+            (291, 0.9, 100, 'E'), (1702, 0.8, 142, 'F'), (1633, 0.7, 100, 'G'), (1086, 0.6, 145, 'H'),(10, 0.2, 10, '88DTT'), (5, 0.1, 2, 'TT85'),
+            (124, 0.6, 100, 'J'), (718, 0.9, 208, 'K'), (976, 0.6, 100, 'L'), (1438, 0.7, 312, 'M'),(1993, 0.25, 338, 'ZDQDSTT'),
+            (910, 1, 198, 'W'), (148, 0.7, 171, 'S'), (1636, 0.9, 117, 'V'), (237, 0.6, 100, 'N'),(3, 0.7, 30, 'DSDTT'), (83, 0.7, 12, 'TT'),
             (771, 0.9, 329, 'Z'), (604, 0.6, 391, 'P'), (1078, 0.6, 100, 'QA'), (640, 0.8, 120, 'O'),
             (1510, 1, 188, 'RE'), (741, 0.6, 271, 'JSU'), (1358, 0.9, 334, 'GT'), (1682, 0.7, 153, 'FA'),
-            (993, 0.7, 130, 'TT'), (99, 0.7, 100, 'MA'), (1068, 0.8, 154, 'SA'), (1669, 1, 289, 'KI')]
+            (993, 0.7, 130, 'TT'),(93, 0.4, 1130, 'TDST'),(93, 0.9, 96, 'TTD'),(1993, 0.7, 130, 'RZD'), (99, 0.7, 100, 'MA'), (1068, 0.8, 154, 'SA'), (1669, 1, 289, 'KI')]
 
-    ga = GeneticAlgorithm(data, max_weight=12210, max_volume=12, max_generations=50, population_size=200) 
+    ga = GeneticAlgorithm(data, max_weight=12210, max_volume=12, max_generations=60, population_size=200, elites=1) 
 
     ga.run()     # run the GA
     print(ga.printFittest()) 
     ga.printEvolutionGraph()
-    
